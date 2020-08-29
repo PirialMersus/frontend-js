5b8cd6bf05c70482169453d5cf8bb15f5b3ff91e
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2020-08-29 17:34:39 +0300

    git hub find user ready

--- githubApi/js/forms.js
+++ githubApi/js/forms.js
@@ -1,42 +1,49 @@
 const form = document.getElementById("formId"),
   input = document.getElementById("inputData"),
-  div = document.getElementsByClassName("user"),
-  user = {};
-// console.log(div[0]);
+  divUser = document.getElementsByClassName("user");
+let user;
+// console.log(divUser[0]);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  divUser[0].innerHTML = "";
+  user = input.value;
 
-  div[0].innerHTML = "";
-  div[0].classList.add("active");
-  // findUserData(input.value);
-  renderUser(input.value);
-  input.value = "";
-});
-
-const renderUser = (nickName) => {
-  fetch(`https://api.github.com/users/${nickName}`)
-    .then((response) => response.json())
-    .then((result) => {
-      div[0].innerHTML = `
-        <a href="${result.html_url}" class="nickNameLink">Ссылка на страничку акаунта</a>
-        <img src="${result.avatar_url}"alt="ava" width="110px"/>
-        <a href="https://github.com/${nickName}?tab=repositories"
-           class="taskLink">Репозитории пользователя (${result.public_repos})</a>
-        <h4> Последняя активность: ${result.updated_at}</h4>
-      `;
+  findUserData(user)
+    .then((userData) => {
+      renderUser(userData, user);
+      input.value = "";
+    })
+    .then(() => {
+      divUser[0].classList.add("active");
     })
     .catch((err) => {
-      div[0].innerHTML = `${err}`;
+      alert(err);
+      divUser[0].classList.remove("active");
     });
-  // .fail(function () {
-  //   $(".user").html("Искомый пользователь не найден");
-  // });
+});
+
+const findUserData = (nickName) => {
+  return fetch(`https://api.github.com/users/${nickName}`)
+    .then((response) => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error("Введенный пользователь не найден");
+      }
+    })
+    .then((user) => {
+      userData = user;
+      return userData;
+    });
 };
 
-function findUserData(nickName) {
-  user = fetch(`https://api.github.com/users/${nickName}`).then((response) =>
-    response.json()
-  );
-  return user;
+function renderUser(userData, nickName) {
+  divUser[0].innerHTML = `
+        <img src="${userData.avatar_url}"alt="ava" width="110px" height="110px"/>
+        <a href="${userData.html_url}" class="nickNameLink">Ссылка на страничку акаунта</a>
+        <a href="https://github.com/${nickName}?tab=repositories"
+          class="taskLink">Репозитории пользователя (${userData.public_repos})</a>
+        <h4> Последняя активность:<br/> ${userData.updated_at}</h4>
+      `;
 }
