295c3d21e5fee82382b794c26ac9367dbf3b1d5a
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2020-09-05 13:10:46 +0300

    50% json data done

--- jsonData/css/style1.css/main.dev.js
+++ jsonData/css/style1.css/main.dev.js
@@ -2,66 +2,28 @@
 
 var input = document.getElementById("inputFile");
 var dataTable = document.getElementById("dataTable");
-var isFormToReductDataOpen = false; // let copyTable = document.getElementById("copyTable");
+var isFormToReductDataOpen = false;
+var selectedRow = "";
+var selectedRowBeforChangingData = "";
+var rowAttribute = 0;
+var data;
+var jsonKeys;
 
-var selectedRow = ""; // copyTable.classList.remove("selected");
-// let dataJSON;
-// input.addEventListener(
-//   "change",
-//   function (event) {
-//     let file = input.files[0];
-//     let reader = new FileReader();
-//     reader.onload = function (event) {
-//       // console.log(reader.result);
-//       dataJSON = JSON.parse(reader.result);
-//       renderJSON(dataJSON);
-//     };
-//     reader.readAsText(file); //????????????зачем эта строка
-//     // let files = input.files;
-//   },
-//   false
-// );
+function onClickSubmitInFormReductRow(rowAttr, values) {
+  for (var i = 0; i < values.length; i++) {
+    data[rowAttr][jsonKeys[i]] = values[i];
+  }
 
-var data = [{
-  candidateName: "Peter Petrov",
-  candidateStatus: "pending",
-  candidateNeedOffer: 1800
-}, {
-  candidateName: "Ivan Ivanov",
-  candidateStatus: "pending",
-  candidateNeedOffer: 3000
-}, {
-  candidateName: "Roman Romanov",
-  candidateStatus: "oninterview",
-  candidateNeedOffer: 2400
-}, {
-  candidateName: "Konstantin Fesenko",
-  candidateStatus: "on_test_perdiod",
-  candidateNeedOffer: 3000
-}, {
-  candidateName: "Roman Konstantinov",
-  candidateStatus: "on_test_perdiod",
-  candidateNeedOffer: 123456
-}, {
-  candidateName: "Konstantin Konstantinov",
-  candidateStatus: "on_test_perdiod",
-  candidateNeedOffer: 3000
-}, {
-  candidateName: "Optre Hgterrw",
-  candidateStatus: "on_test_perdiod",
-  candidateNeedOffer: 5678
-}, {
-  candidateName: "Omg Ksdsfnov",
-  candidateStatus: "on_prison",
-  candidateNeedOffer: 3000
-}, {
-  candidateName: "Naz Rtu",
-  candidateStatus: "validate",
-  candidateNeedOffer: 200
-}];
+  renderJSON(data);
+  isFormToReductDataOpen = false;
+}
+
+function onClickResetInFormReductRow() {
+  selectedRow.innerHTML = selectedRowBeforChangingData;
+  isFormToReductDataOpen = false;
+}
 
 function renderJSON(data) {
-  var jsonKeys = Object.keys(data[0]);
   var dataForRender,
       tableForRender = "";
 
@@ -76,7 +38,6 @@ function renderJSON(data) {
     var tempLine = "";
 
     for (var _j = 0; _j < jsonKeys.length; _j++) {
-      // console.log(data[i][jsonKeys[j]]);
       if (_j === jsonKeys.length - 1) {
         tempLine += "<td class=\"lastTd\" data-row=\"".concat(i, "\" data-col=\"").concat(_j, "\">").concat(data[i][jsonKeys[_j]], "<div class=\"pen\"></div></td>");
       } else {
@@ -87,58 +48,74 @@ function renderJSON(data) {
     tableForRender += "<tr>".concat(tempLine, "</tr>");
   }
 
-  dataForRender = "\n      <caption>\n        \u0414\u0430\u043D\u043D\u044B\u0435 \u0438\u0437 \u0444\u0430\u0439\u043B\u0430\n      </caption>\n      ".concat(dataForRender, "\n      ").concat(tableForRender, "\n    ");
-  dataTable.innerHTML = dataForRender; // console.log(dataForRender);
+  dataForRender = "\n  <caption>\n  \u0414\u0430\u043D\u043D\u044B\u0435 \u0438\u0437 \u0444\u0430\u0439\u043B\u0430\n  </caption>\n  ".concat(dataForRender, "\n  ").concat(tableForRender, "\n  ");
+  dataTable.innerHTML = dataForRender;
 }
 
-renderJSON(data); // renderCopyTable(data);
+input.addEventListener("change", function (event) {
+  var file = input.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function (event) {
+    data = JSON.parse(reader.result);
+    jsonKeys = Object.keys(data[0]);
+    renderJSON(data);
+  };
+
+  reader.readAsText(file); //????????????зачем эта строка
+}, false);
 
 dataTable.onclick = function (event) {
+  var row = "";
   if (isFormToReductDataOpen) return;
-  isFormToReductDataOpen = true;
-  var jsonKeys = Object.keys(data[0]);
-  var thAll = document.querySelectorAll("th[data-number]");
+  var thAll = document.querySelectorAll("th");
   var numberOfCols = 0;
-  var td = event.target.closest("td"); // (1)
-
-  if (!td) return; // (2)
-
-  if (!dataTable.contains(td)) return; // (3)
-
-  if (selectedRow) {
-    selectedRow.classList.remove("selected");
-  }
-
+  var td = event.target.closest("td");
+  if (!td) return;
+  if (!dataTable.contains(td)) return;
+  isFormToReductDataOpen = true;
+  selectedRowBeforChangingData = td.parentNode.innerHTML;
   var height = td.parentNode.clientHeight;
   var width = td.parentNode.clientWidth;
   selectedRow = td.parentNode;
-  var rowAttribute = td.getAttribute("data-row");
-  selectedRow.classList.add("selected");
-  selectedRow.style.height = height + "px";
-  selectedRow.style.width = width + "px";
-  selectedRow.style.position = "relative"; // <form action="">
-  //   <input type="text" value="${data[rowAttribute][jsonKeys[j]]}" />
-  // </form>;
-
-  var row = "";
+  rowAttribute = td.getAttribute("data-row");
 
   for (var j = 0; j < jsonKeys.length; j++) {
-    row += "<input type=\"text\" class=\"inputFormReduct\" value=\"".concat(data[rowAttribute][jsonKeys[j]], "\"/>");
+    row += "<input type=\"text\" name=\"".concat(j, "\" class=\"inputFormReduct\" value=\"").concat(data[rowAttribute][jsonKeys[j]], "\"/>");
     numberOfCols = j;
   }
 
-  var dataForInputRow = "<td>\n    <form id=\"formReductRow\" action=\"#\">".concat(row, "\n    <div class=\"wrapperButtons\">\n          <button type=\"submit\" class=\"iconsGeneralRulls saveIcon\"></button>\n          <button class=\"iconsGeneralRulls cancelIcon\"></button>\n        </div>\n    </form>\n  </td>\n  <td></td>\n  <td></td>");
+  var dataForInputRow = "<td>\n    <form id=\"formReductRow\" action=\"#\">".concat(row, "\n    <div class=\"wrapperButtons\">\n          <button type=\"submit\" class=\"iconsGeneralRulls saveIcon\"></button>\n          <button type=\"reset\" class=\"iconsGeneralRulls cancelIcon\"></button>\n        </div>\n    </form>\n  </td>\n  ");
   selectedRow.innerHTML = dataForInputRow;
+  selectedRow.style.height = height + "px";
+  selectedRow.style.width = width + "px";
   document.getElementById("formReductRow").style.height = height + "px";
   document.getElementById("formReductRow").style.width = width - numberOfCols + "px";
   var inputs = document.getElementsByClassName("inputFormReduct");
+  var inputsValues = [];
 
   for (var i = 0; i < inputs.length; i++) {
     inputs[i].style.width = thAll[i].clientWidth - 1 + "px";
+    inputsValues[i] = inputs[i].value;
   }
+
+  document.getElementById("formReductRow").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    for (var _i = 0; _i < inputs.length; _i++) {
+      inputsValues[_i] = e.srcElement[_i].value;
+    }
+
+    onClickSubmitInFormReductRow(rowAttribute, inputsValues);
+  });
+  document.getElementById("formReductRow").addEventListener("reset", function (e) {
+    onClickResetInFormReductRow();
+  });
 };
 
-document.getElementById("formReductRow").addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log("working");
-});
+document.getElementById("save").onclick = function () {
+  var text = JSON.stringify(data);
+  this.href = "data:attachment/text," + encodeURI(text);
+  this.target = "_blank";
+  this.download = "filename.json";
+};
--- jsonData/js/main.js
+++ jsonData/js/main.js
@@ -1,77 +1,30 @@
-let input = document.getElementById("inputFile");
-let dataTable = document.getElementById("dataTable");
+const input = document.getElementById("inputFile");
+const dataTable = document.getElementById("dataTable");
+
 let isFormToReductDataOpen = false;
-// let copyTable = document.getElementById("copyTable");
 let selectedRow = "";
-// copyTable.classList.remove("selected");
-// let dataJSON;
-// input.addEventListener(
-//   "change",
-//   function (event) {
-//     let file = input.files[0];
-//     let reader = new FileReader();
-//     reader.onload = function (event) {
-//       // console.log(reader.result);
-//       dataJSON = JSON.parse(reader.result);
-//       renderJSON(dataJSON);
-//     };
-//     reader.readAsText(file); //????????????зачем эта строка
-//     // let files = input.files;
-//   },
-//   false
-// );
-const data = [
-  {
-    candidateName: "Peter Petrov",
-    candidateStatus: "pending",
-    candidateNeedOffer: 1800,
-  },
-  {
-    candidateName: "Ivan Ivanov",
-    candidateStatus: "pending",
-    candidateNeedOffer: 3000,
-  },
-  {
-    candidateName: "Roman Romanov",
-    candidateStatus: "oninterview",
-    candidateNeedOffer: 2400,
-  },
-  {
-    candidateName: "Konstantin Fesenko",
-    candidateStatus: "on_test_perdiod",
-    candidateNeedOffer: 3000,
-  },
-  {
-    candidateName: "Roman Konstantinov",
-    candidateStatus: "on_test_perdiod",
-    candidateNeedOffer: 123456,
-  },
-  {
-    candidateName: "Konstantin Konstantinov",
-    candidateStatus: "on_test_perdiod",
-    candidateNeedOffer: 3000,
-  },
-  {
-    candidateName: "Optre Hgterrw",
-    candidateStatus: "on_test_perdiod",
-    candidateNeedOffer: 5678,
-  },
-  {
-    candidateName: "Omg Ksdsfnov",
-    candidateStatus: "on_prison",
-    candidateNeedOffer: 3000,
-  },
-  {
-    candidateName: "Naz Rtu",
-    candidateStatus: "validate",
-    candidateNeedOffer: 200,
-  },
-];
+let selectedRowBeforChangingData = "";
+let rowAttribute = 0;
+let data;
+let jsonKeys;
+
+function onClickSubmitInFormReductRow(rowAttr, values) {
+  for (let i = 0; i < values.length; i++) {
+    data[rowAttr][jsonKeys[i]] = values[i];
+  }
+  renderJSON(data);
+  isFormToReductDataOpen = false;
+}
+
+function onClickResetInFormReductRow() {
+  selectedRow.innerHTML = selectedRowBeforChangingData;
+  isFormToReductDataOpen = false;
+}
 
 function renderJSON(data) {
-  const jsonKeys = Object.keys(data[0]);
   let dataForRender,
     tableForRender = "";
+
   for (let j = 0; j < jsonKeys.length; j++) {
     tableForRender += `<th data-number>${jsonKeys[j]}</th>`;
   }
@@ -81,7 +34,6 @@ function renderJSON(data) {
   for (let i = 0; i < data.length; i++) {
     let tempLine = "";
     for (let j = 0; j < jsonKeys.length; j++) {
-      // console.log(data[i][jsonKeys[j]]);
       if (j === jsonKeys.length - 1) {
         tempLine += `<td class="lastTd" data-row="${i}" data-col="${j}">${
           data[i][jsonKeys[j]]
@@ -95,51 +47,48 @@ function renderJSON(data) {
     tableForRender += `<tr>${tempLine}</tr>`;
   }
   dataForRender = `
-      <caption>
-        Данные из файла
-      </caption>
-      ${dataForRender}
-      ${tableForRender}
-    `;
+  <caption>
+  Данные из файла
+  </caption>
+  ${dataForRender}
+  ${tableForRender}
+  `;
   dataTable.innerHTML = dataForRender;
-  // console.log(dataForRender);
 }
 
-renderJSON(data);
-// renderCopyTable(data);
+input.addEventListener(
+  "change",
+  function (event) {
+    let file = input.files[0];
+    let reader = new FileReader();
+    reader.onload = function (event) {
+      data = JSON.parse(reader.result);
+      jsonKeys = Object.keys(data[0]);
+      renderJSON(data);
+    };
+    reader.readAsText(file); //????????????зачем эта строка
+  },
+  false
+);
 
 dataTable.onclick = function (event) {
+  let row = "";
+
   if (isFormToReductDataOpen) return;
-  isFormToReductDataOpen = true;
-  const jsonKeys = Object.keys(data[0]);
-  const thAll = document.querySelectorAll("th[data-number]");
+  const thAll = document.querySelectorAll("th");
   let numberOfCols = 0;
-  let td = event.target.closest("td"); // (1)
-
-  if (!td) return; // (2)
-
-  if (!dataTable.contains(td)) return; // (3)
-
-  if (selectedRow) {
-    selectedRow.classList.remove("selected");
-  }
+  let td = event.target.closest("td");
+  if (!td) return;
+  if (!dataTable.contains(td)) return;
+  isFormToReductDataOpen = true;
+  selectedRowBeforChangingData = td.parentNode.innerHTML;
   const height = td.parentNode.clientHeight;
   const width = td.parentNode.clientWidth;
   selectedRow = td.parentNode;
-  const rowAttribute = td.getAttribute("data-row");
-  selectedRow.classList.add("selected");
-  selectedRow.style.height = height + "px";
-  selectedRow.style.width = width + "px";
-  selectedRow.style.position = "relative";
-
-  // <form action="">
-  //   <input type="text" value="${data[rowAttribute][jsonKeys[j]]}" />
-  // </form>;
-
-  let row = "";
+  rowAttribute = td.getAttribute("data-row");
 
   for (let j = 0; j < jsonKeys.length; j++) {
-    row += `<input type="text" class="inputFormReduct" value="${
+    row += `<input type="text" name="${j}" class="inputFormReduct" value="${
       data[rowAttribute][jsonKeys[j]]
     }"/>`;
     numberOfCols = j;
@@ -148,25 +97,43 @@ dataTable.onclick = function (event) {
     <form id="formReductRow" action="#">${row}
     <div class="wrapperButtons">
           <button type="submit" class="iconsGeneralRulls saveIcon"></button>
-          <button class="iconsGeneralRulls cancelIcon"></button>
+          <button type="reset" class="iconsGeneralRulls cancelIcon"></button>
         </div>
     </form>
   </td>
-  <td></td>
-  <td></td>`;
+  `;
 
   selectedRow.innerHTML = dataForInputRow;
+  selectedRow.style.height = height + "px";
+  selectedRow.style.width = width + "px";
   document.getElementById("formReductRow").style.height = height + "px";
   document.getElementById("formReductRow").style.width =
     width - numberOfCols + "px";
 
   const inputs = document.getElementsByClassName("inputFormReduct");
+  const inputsValues = [];
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].style.width = thAll[i].clientWidth - 1 + "px";
+    inputsValues[i] = inputs[i].value;
   }
+
+  document.getElementById("formReductRow").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    for (let i = 0; i < inputs.length; i++) {
+      inputsValues[i] = e.srcElement[i].value;
+    }
+    onClickSubmitInFormReductRow(rowAttribute, inputsValues);
+  });
+
+  document.getElementById("formReductRow").addEventListener("reset", (e) => {
+    onClickResetInFormReductRow();
+  });
 };
 
-document.getElementById("formReductRow").addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("working");
-});
+document.getElementById("save").onclick = function () {
+  let text = JSON.stringify(data);
+  this.href = "data:attachment/text," + encodeURI(text);
+  this.target = "_blank";
+  this.download = "filename.json";
+};
