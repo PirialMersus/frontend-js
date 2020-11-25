5287420defac5a5b576a26f867a4a5162ba1decf
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2020-11-25 11:54:37 +0300

    json data mobile olmost done

--- jsonData/css/style1.css/label.dev.js
+++ jsonData/css/style1.css/label.dev.js
@@ -14,19 +14,18 @@ Array.prototype.forEach.call(inputs, function (input) {
       label.innerHTML = labelVal;
     }
   });
-});
-var buttons = document.querySelectorAll(".classForEditingButtons");
-buttons.forEach(function (btn) {
-  btn.addEventListener("mouseenter", function (e) {
-    var x = e.clientX - e.target.offsetLeft;
-    var y = e.clientY - e.target.offsetTop;
-    var ripples = document.createElement("span");
-    ripples.style.left = x + "px";
-    ripples.style.top = y + "px";
-    ripples.className = "tempSpan";
-    this.appendChild(ripples);
-    setTimeout(function () {
-      ripples.remove();
-    }, 1000);
-  });
-});
+}); // const buttons = document.querySelectorAll(".classForEditingButtons");
+// buttons.forEach((btn) => {
+//   btn.addEventListener("mouseenter", function (e) {
+//     let x = e.clientX - e.target.offsetLeft;
+//     let y = e.clientY - e.target.offsetTop;
+//     let ripples = document.createElement("span");
+//     ripples.style.left = x + "px";
+//     ripples.style.top = y + "px";
+//     ripples.className = "tempSpan";
+//     this.appendChild(ripples);
+//     setTimeout(() => {
+//       ripples.remove();
+//     }, 1000);
+//   });
+// });
--- jsonData/js/label.js
+++ jsonData/js/label.js
@@ -20,20 +20,20 @@ Array.prototype.forEach.call(inputs, function (input) {
   });
 });
 
-const buttons = document.querySelectorAll(".classForEditingButtons");
-buttons.forEach((btn) => {
-  btn.addEventListener("mouseenter", function (e) {
-    let x = e.clientX - e.target.offsetLeft;
-    let y = e.clientY - e.target.offsetTop;
+// const buttons = document.querySelectorAll(".classForEditingButtons");
+// buttons.forEach((btn) => {
+//   btn.addEventListener("mouseenter", function (e) {
+//     let x = e.clientX - e.target.offsetLeft;
+//     let y = e.clientY - e.target.offsetTop;
 
-    let ripples = document.createElement("span");
-    ripples.style.left = x + "px";
-    ripples.style.top = y + "px";
-    ripples.className = "tempSpan";
-    this.appendChild(ripples);
+//     let ripples = document.createElement("span");
+//     ripples.style.left = x + "px";
+//     ripples.style.top = y + "px";
+//     ripples.className = "tempSpan";
+//     this.appendChild(ripples);
 
-    setTimeout(() => {
-      ripples.remove();
-    }, 1000);
-  });
-});
+//     setTimeout(() => {
+//       ripples.remove();
+//     }, 1000);
+//   });
+// });
